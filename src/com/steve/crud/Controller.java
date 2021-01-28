34547bc7e9e466f788dea04c61bc465b96e8df24
SteveT90 <stevetolvaj@gmail.com>
2021-01-28 07:24:47 -0500

    Updated UI

--- src/com/steve/crud/Controller.java
+++ src/com/steve/crud/Controller.java
@@ -138,7 +138,6 @@ public class Controller {
     }
 
     private void updateRecord() {
-        // TODO fix query to update all fields related to a name but keep the uuid the same for future reference if needed.
         Date getDateFromPicker = java.sql.Date.valueOf(textFieldDOB.getValue());
         String query = "UPDATE cats SET dob = '" + getDateFromPicker +
                 "', breed = '" + textFieldBreed.getText() + "', weight = '" + textFieldWeight.getText() +
