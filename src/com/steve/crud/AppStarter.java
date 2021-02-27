56076604c612d940e3fb881964b8cd1d2db67570
SteveT90 <stevetolvaj@gmail.com>
2021-02-27 18:18:00 -0500

    Updated with jar file.

---
+++ src/com/steve/crud/AppStarter.java
@@ -0,0 +1,11 @@
+package com.steve.crud;
+
+/**
+ * The AppStarter class will call the main method in Main class. This allows the app to run via jar file.
+ */
+
+public class AppStarter {
+    public static void main(String[] args) {
+        com.steve.crud.Main.main(args);
+    }
+}
